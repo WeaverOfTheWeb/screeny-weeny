77527dbbe4ef7bfc61cea916abd3e2007a1bd9ff
Lewis <1872377+WeaverOfTheWeb@users.noreply.github.com>
2021-02-03 08:18:14 +0000

    v1

---
+++ screenshot.js
@@ -0,0 +1,39 @@
+const puppeteer = require("puppeteer");
+
+module.exports = function ({url, height, width}) {
+	return new Promise((resolve, reject) => {
+		(async () => {
+			const browser = await puppeteer.launch({
+				args: [
+					"--incognito",
+					"--no-sandbox",
+					"--disable-setuid-sandbox"
+				]
+			});
+
+			const page = await browser.newPage();
+
+			await page.setViewport({
+				height: parseInt(height) || 480,
+				width: parseInt(width) || 640,
+			});
+
+			await page.goto(url, {
+				waitUntil: [
+					"load",
+					"networkidle0",
+					"domcontentloaded"
+				]
+			});
+
+			const buffer = await page.screenshot({
+				fullPage: false,
+				type: "png"
+			});
+
+			await browser.close();
+
+			resolve(buffer);
+		})();
+	});
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+const screenshot = require("./screenshot");
+const http = require("http");
+const url = require("url");
+
+http.createServer(async function (req, res) {
+	const queryObject = url.parse(req.url, true).query;
+
+	if (queryObject.url && queryObject.height && queryObject.width) {
+		try {
+			const buffer = await screenshot({
+				height: queryObject.height,
+				width: queryObject.width,
+				url: queryObject.url
+			});
+
+			res.writeHead(
+				200,
+				{ "Content-Type": "text/html" }
+			);
+
+			res.write(`<img src="data:image/png;base64,${buffer.toString("base64")}" />`);
+		} catch (e) {
+			res.writeHead(
+				500,
+				{ "Content-Type": "text/html" }
+			);
+			res.write(`${e}`);
+		}
+	} else {
+		res.write(JSON.stringify(queryObject));
+	}
+
+	res.end();
+}).listen(8080);
