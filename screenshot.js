542d91386f9338ff98da1a9e07dddcb9e01d8686
WeaverOfTheWeb <lewis.d.b.shaw@gmail.com>
2021-08-10 00:12:45 +0100

    Repo file cleanup

---
+++ screenshot.js
@@ -0,0 +1,36 @@
+const puppeteer = require("puppeteer");
+
+module.exports = function ({ url, height, width, format }) {
+	return new Promise((resolve, reject) => {
+		(async () => {
+			const browser = await puppeteer.launch({
+				args: [
+					"--incognito",
+					"--no-sandbox",
+					"--disable-setuid-sandbox",
+				],
+				headless: true,
+			});
+
+			const page = await browser.newPage();
+
+			await page.setViewport({
+				height: parseInt(height) || 480,
+				width: parseInt(width) || 640,
+			});
+
+			await page.goto(url, {
+				waitUntil: ["load", "networkidle0", "domcontentloaded"],
+			});
+
+			const buffer = await page.screenshot({
+				fullPage: false,
+				type: format || "png",
+			});
+
+			await browser.close();
+
+			resolve(buffer);
+		})();
+	});
+};
