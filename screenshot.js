a86579d11a41c9e5cfedad3562a4703ef5d9bac0
WeaverOfTheWeb <lewis.d.b.shaw@gmail.com>
2021-08-22 02:03:22 +0100

    Added quality attribute for JPEG screenshots

--- screenshot.js
+++ screenshot.js
@@ -25,6 +25,7 @@ module.exports = function ({ url, height, width, format }) {
 
 			const buffer = await page.screenshot({
 				fullPage: false,
+				quality: 70,
 				type: format || "png",
 			});
 
